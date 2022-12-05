42bfca0c4bec774fefaa8d800bf8142e8ade805a
Fred Bradley <code@fredbradley.co.uk>
2022-12-05 14:30:01 +0000

    refactor: add specific method for searching by ip address

--- src/Traits/Search.php
+++ src/Traits/Search.php
@@ -9,8 +9,9 @@ namespace FredBradley\XirrusApi\Traits;
 trait Search
 {
     /**
-     * @param string $search_query
-     * @param string|null $search_type
+     * @param  string  $search_query
+     * @param  string|null  $search_type
+     *
      * @return mixed
      */
     public function search(string $search_query, string $search_type = null)
@@ -18,5 +19,24 @@ trait Search
         $uri = $this->generateEndpoint(['search.json', $search_type, $search_query]);
 
         return $this->request('GET', $uri);
+    }
+
+
+    /**
+     * @param  string  $ipAddress
+     *
+     * @return object|null
+     */
+    public function searchByIp(string $ipAddress): ?object
+    {
+        $results = $this->search($ipAddress, 'stations');
+        $data = collect($results->data);
+        if ($data->isNotEmpty()) {
+            $result = $data->where('stationIpAddress', '=', $ipAddress);
+            if ($result->count() === 1) {
+                return $result->first();
+            }
+        }
+        return null;
     }
 }
